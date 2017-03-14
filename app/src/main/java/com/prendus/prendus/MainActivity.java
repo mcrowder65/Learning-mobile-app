8a6ece928e2b51f29c03c9a93149b171eda3f0a4
Matt Crowder <mcrowder65@gmail.com>
2017-03-14 15:41:47 -0600

    finally got something up. idk what i even did

--- app/src/main/java/com/prendus/prendus/MainActivity.java
+++ app/src/main/java/com/prendus/prendus/MainActivity.java
@@ -2,6 +2,7 @@ package com.prendus.prendus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the toolbar view inside the activity layout
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Abstract to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+//        setSupportActionBar(toolbar);
     }
 }
