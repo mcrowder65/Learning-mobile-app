fe09b2c7172fd50dd304c3cd4f99a6303f0575e0
Matt Crowder <mcrowder65@gmail.com>
2017-03-14 18:32:28 -0600

    finally got the toolbar

--- app/src/main/java/com/prendus/prendus/MainActivity.java
+++ app/src/main/java/com/prendus/prendus/MainActivity.java
@@ -19,5 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 
 }
