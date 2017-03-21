54c0306f64692c44e97db290f8edad813153e99c
Matt Crowder <mcrowder65@gmail.com>
2017-03-21 14:24:02 -0600

    added popup menu. doesn't work completely yet

--- app/src/main/java/com/prendus/prendus/activities/MainActivity.java
+++ app/src/main/java/com/prendus/prendus/activities/MainActivity.java
@@ -1,18 +1,20 @@
-package com.prendus.prendus;
+package com.prendus.prendus.activities;
 
-import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-import com.prendus.prendus.activities.dropdown.DropDownMenuActivity;
+import com.prendus.prendus.R;
+import com.prendus.prendus.utilities.Utilities;
 
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
         // Make sure the toolbar exists in the activity and is not null
         setSupportActionBar(toolbar);
         makeSnackBar("init!");
-
     }
 
     // Menu icons are inflated just as they were with actionbar
@@ -45,9 +46,19 @@ public class MainActivity extends AppCompatActivity {
 //        Intent i = new Intent(getApplicationContext(), LoginActivity.class);
 //        startActivity(i);
     }
+
     public void dropDownMenuClicked(MenuItem item) {
-        Intent i = new Intent(getApplicationContext(), DropDownMenuActivity.class);
-        startActivity(i);
+        View menuItemView = findViewById(R.id.menu);
+        PopupMenu popup = new PopupMenu(MainActivity.this, menuItemView);
+        popup.getMenuInflater().inflate(Utilities.isLoggedIn() ?
+                R.menu.logged_in_menu : R.menu.logged_out_menu, popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(MainActivity.this,"You Clicked : " + item.getTitle(),Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        popup.show();//showing popup menu
     }
 
 }
--- app/src/main/java/com/prendus/prendus/utilities/Utilities.java
+++ app/src/main/java/com/prendus/prendus/utilities/Utilities.java
@@ -1,5 +1,6 @@
 package com.prendus.prendus.utilities;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.gson.Gson;
 import com.prendus.prendus.firebase.Firebase;
 
@@ -10,4 +11,12 @@ import com.prendus.prendus.firebase.Firebase;
 public class Utilities {
     public static Firebase firebase = new Firebase();
     public static Gson g = new Gson();
+    public static boolean isLoggedIn() {
+        final FirebaseAuth auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
