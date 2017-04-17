24a965d1dac09484af155f43ebbfbd8b34ddcb09
Matt Crowder <mcrowder65@gmail.com>
2017-04-17 10:05:22 -0600

    added shadow to questions

--- app/src/main/java/com/prendus/prendus/activities/takequiz/TakeQuizActivity.java
+++ app/src/main/java/com/prendus/prendus/activities/takequiz/TakeQuizActivity.java
@@ -39,44 +39,52 @@ public class TakeQuizActivity extends PrendusActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_take_quiz);
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_take_quiz);
 
-        // Find the toolbar view inside the activity layout
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        // Sets the Abstract to act as the ActionBar for this Activity window.
-        // Make sure the toolbar exists in the activity and is not null
-        setSupportActionBar(toolbar);
-        String quizAsJson = getIntent().getStringExtra(IntentExtras.QUIZ_AS_JSON);
-        quiz = Utilities.g.fromJson(quizAsJson, Quiz.class);
-        if (!quizAsJson.equals(quiz.toJson())) {
-            Utilities.log("Something is going horribly wrong in take quiz...");
+            // Find the toolbar view inside the activity layout
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            // Sets the Abstract to act as the ActionBar for this Activity window.
+            // Make sure the toolbar exists in the activity and is not null
+            setSupportActionBar(toolbar);
+            String quizAsJson = getIntent().getStringExtra(IntentExtras.QUIZ_AS_JSON);
+            quiz = Utilities.g.fromJson(quizAsJson, Quiz.class);
+            if (!quizAsJson.equals(quiz.toJson())) {
+                Utilities.log("Something is going horribly wrong in take quiz...");
+            }
+            quizTitle = (TextView) findViewById(R.id.quizTitle);
+            quizQuestion = (TextView) findViewById(R.id.quizQuestion);
+            nextQuestion = (Button) findViewById(R.id.nextQuestion);
+            quizResults = (TextView) findViewById(R.id.quizResults);
+            answer = (EditText) findViewById(R.id.answer);
+            currentQuestionNumber = (TextView) findViewById(R.id.currentQuestionNumber);
+            thumbUp = (ImageView) findViewById(R.id.thumbup);
+            thumbDown = (ImageView) findViewById(R.id.thumbdown);
+            quizScore = (TextView) findViewById(R.id.quizScore);
+            TakeQuizActivity self = this;
+            takeQuizManipulator = new TakeQuizManipulator(self);
+            takeQuizManipulator.manipulate();
+            simpleViewAnimator = (ViewAnimator) findViewById(R.id.simpleViewAnimator); //get the reference of ViewAnimator
+
+            slide_in_left = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+            slide_out_right = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+
+            simpleViewAnimator.setInAnimation(slide_out_right);
+            simpleViewAnimator.setOutAnimation(slide_out_right);
+        } catch (Exception e) {
+            Utilities.log(e);
         }
-        quizTitle = (TextView) findViewById(R.id.quizTitle);
-        quizQuestion = (TextView) findViewById(R.id.quizQuestion);
-        nextQuestion = (Button) findViewById(R.id.nextQuestion);
-        quizResults = (TextView) findViewById(R.id.quizResults);
-        answer = (EditText) findViewById(R.id.answer);
-        currentQuestionNumber = (TextView) findViewById(R.id.currentQuestionNumber);
-        thumbUp = (ImageView) findViewById(R.id.thumbup);
-        thumbDown = (ImageView) findViewById(R.id.thumbdown);
-        quizScore = (TextView) findViewById(R.id.quizScore);
-        TakeQuizActivity self = this;
-        takeQuizManipulator = new TakeQuizManipulator(self);
-        takeQuizManipulator.manipulate();
-        simpleViewAnimator = (ViewAnimator) findViewById(R.id.simpleViewAnimator); //get the reference of ViewAnimator
 
-        slide_in_left = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
-        slide_out_right = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
-
-        simpleViewAnimator.setInAnimation(slide_out_right);
-        simpleViewAnimator.setOutAnimation(slide_out_right);
     }
 
     public void nextQuestionClicked(View view) {
-
-        simpleViewAnimator.showNext();
-        takeQuizManipulator.nextQuestion();
+        try {
+            simpleViewAnimator.showNext();
+            takeQuizManipulator.nextQuestion();
+        } catch (Exception e) {
+            Utilities.log(e);
+        }
     }
 
 }
